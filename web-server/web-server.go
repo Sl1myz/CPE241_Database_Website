2c7f32850ec77a585fce12d7bba9fc6c44c28f2b
Sl1myz <147938695+Sl1myz@users.noreply.github.com>
2025-04-22 14:30:48 +0700

    Initialization of redirecting port 8080 to index.html

---
+++ web-server/web-server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func startServer() {
+	// Serve the index.html file directly when accessing the root URL
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "web-server/index.html")
+		} else {
+			http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+		}
+	})
+
+	fmt.Println("Serving on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	startServer()
+}
